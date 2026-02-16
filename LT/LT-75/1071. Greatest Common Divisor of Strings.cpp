b6a2e5cbd260c9e695c3df26e44e58e7d1ac8e5f
MoriamAkterSwarna <moriamakterswarna@gmail.com>
2026-02-16 09:39:13 +0600

    lt target done

---
+++ LT/LT-75/1071. Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,22 @@
+
+
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75 
+
+
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+       
+        if(str1 + str2 != str2 + str1 ){
+            return "" ; 
+        }
+
+        int result = gcd(str1.length(), str2.length()) ;
+
+
+        return str1.substr(0, result); 
+        
+    }
+};
